d99ad593099df8c62572ae573234ca9eb922b260
ubuntu <vps>
2022-11-07 07:17:54 +0000

    Revert "Settings: Redesign 5G switch"
    This reverts commit 1251aa5129af91259471ae7811f57addf1611e38.

--- src/com/android/settings/network/telephony/PreferredNetworkModePreferenceController.java
+++ src/com/android/settings/network/telephony/PreferredNetworkModePreferenceController.java
@@ -50,7 +50,6 @@ public class PreferredNetworkModePreferenceController extends TelephonyBasePrefe
 
     private static final String LOG_TAG = "PreferredNetworkMode";
     private CarrierConfigManager mCarrierConfigManager;
-    private ContentObserver mPreferredNetworkModeObserver;
     private ContentObserver mSubsidySettingsObserver;
     private TelephonyManager mTelephonyManager;
     private PersistableBundle mPersistableBundle;
@@ -77,15 +76,6 @@ public class PreferredNetworkModePreferenceController extends TelephonyBasePrefe
                     if (PrimaryCardAndSubsidyLockUtils.DBG) {
                         Log.d(LOG_TAG, "mSubsidySettingsObserver#onChange");
                     }
-                    updateState(mPreference);
-                }
-            }
-        };
-        mPreferredNetworkModeObserver = new ContentObserver(new Handler(Looper.getMainLooper())) {
-            @Override
-            public void onChange(boolean selfChange) {
-                if (mPreference != null) {
-                    Log.d(LOG_TAG, "mPreferredNetworkModeObserver#onChange");
                     updateState(mPreference);
                 }
             }
@@ -126,9 +116,6 @@ public class PreferredNetworkModePreferenceController extends TelephonyBasePrefe
                     Settings.Secure.getUriFor(PrimaryCardAndSubsidyLockUtils.SUBSIDY_STATUS), false,
                     mSubsidySettingsObserver);
         }
-        mContext.getContentResolver().registerContentObserver(
-                Settings.Global.getUriFor(Settings.Global.PREFERRED_NETWORK_MODE + mSubId), true,
-                mPreferredNetworkModeObserver);
     }
 
     @OnLifecycleEvent(ON_STOP)
@@ -138,9 +125,6 @@ public class PreferredNetworkModePreferenceController extends TelephonyBasePrefe
         }
         if (mSubsidySettingsObserver != null) {
             mContext.getContentResolver().unregisterContentObserver(mSubsidySettingsObserver);
-        }
-        if (mPreferredNetworkModeObserver != null) {
-            mContext.getContentResolver().unregisterContentObserver(mPreferredNetworkModeObserver);
         }
     }
 
